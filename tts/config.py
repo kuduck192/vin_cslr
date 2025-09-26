7023795fcdad65663dad48883278b1f46ce60aa5
heathcliff <dat.tranheathcliff@hcmut.edu.vn>
2025-09-26 17:33:45 +0700

    Update: config for tts module

---
+++ tts/config.py
@@ -0,0 +1,6 @@
+API_KEY   = "sk_c4af69d0f3ab03562312908e2d2b6d485e2926c437277171"  
+VOICE_ID  = "21m00Tcm4TlvDq8ikWAM" 
+MODEL_ID  = "eleven_multilingual_v2"  
+SR       = 22050 
+CHANNELS = 1
+SAMPWIDTH = 2   
